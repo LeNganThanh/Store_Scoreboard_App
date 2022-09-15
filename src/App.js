e8245de47cee1563360eca604946ed9c5f857464
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-09-15 14:16:05 +0200

    Move Infoboard to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Scoreboard from "./Components/Scoreboard";
 import ScoreContextProvider from "./Contexts/ScoreContext";
+import Infoboard from "./Components/Infoboard";
 
 function App() {
   return (
     <ScoreContextProvider>
       <div className="App">
         <h1>Score Board</h1>
-        <Scoreboard/>
+        <Scoreboard />
+        <Infoboard />
       </div>
     </ScoreContextProvider>
   );
